14edce426e88f8a0543dd4f5e1ba2736722ea642
Emma Kang <emma140626@gmail.com>
2023-04-16 19:54:11 -0400

    Initial commit

---
+++ src/main/java/com/ekang/sdp/rest/StudentPortalRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ekang.sdp.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPortalRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPortalRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ekang/sdp/rest/StudentPortalRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekang.sdp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPortalRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
